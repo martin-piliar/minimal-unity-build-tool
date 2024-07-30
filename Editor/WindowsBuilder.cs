8bad2549cbebd15ce25200604adcb483d249fb83
Martin Piliar <edino525@gmail.com>
2024-07-30 08:05:37 +0200

    FIX: Windows build path + file extension

--- Editor/WindowsBuilder.cs
+++ Editor/WindowsBuilder.cs
@@ -1,13 +1,21 @@
+using System.IO;
 using UnityEditor;
 
 public class WindowsBuilder : Builder
 {
     protected override string PlatformName => "Windows";
     protected override BuildTarget BuildTarget => BuildTarget.StandaloneWindows64;
+    protected override string FileExtension => ".exe";
 
     [MenuItem("Build/Windows")]
     public static void BuildWindows()
     {
         new WindowsBuilder().Build();
+    }
+
+    protected override string GetBuildPath(string buildName)
+    {
+        string projectName = PlayerSettings.productName;
+        return Path.Combine("Builds", PlatformName, buildName, $"{projectName}{FileExtension}");
     }
 }
